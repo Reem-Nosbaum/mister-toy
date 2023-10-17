cf1f27b7a380152402dad3796a3a96a78c1cf1cf
Reem-Nosbaum <reemnu@gmail.com>
2023-10-17 11:47:40 +0300

    Improve structure / format of the code. :art:

---
+++ frontend-mister-toy/src/components/NavBar.jsx
@@ -0,0 +1,5 @@
+function NavBar() {
+  return <nav className="w-full h-full bg-red-700 text-xl3 ">navbar</nav>;
+}
+
+export default NavBar;
---
+++ frontend-mister-toy/src/routes/Routes.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import MainLayout from "./MainLayout";
+import Login from "../pages/Login";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
