f8d3cac4a4de97bbfec9ec98e0accbd3858683a7
Reem-Nosbaum <reemnu@gmail.com>
2023-10-27 10:35:46 +0300

    Improve structure / format of the code. :art:

--- frontend-mister-toy/src/components/NavBar.jsx
+++ frontend-mister-toy/src/components/NavBar.jsx
@@ -3,26 +3,21 @@ import { useState } from "react";
 import logo from "../assets/images/site-logo.svg";
 import heart from "../assets/images/wishlist.svg";
 import user from "../assets/images/user.svg";
-import cart from "../assets/images/shopping-cart.svg";
+import cart from "../assets/images/bag.svg";
 
 function NavBar() {
   const [isSearchFocused, setIsSearchFocused] = useState(false);
-  const [searchValue, setSearchValue] = useState("");
-
-  const handleInputChange = (e) => {
-    setSearchValue(e.target.value);
-  };
 
   return (
-    <nav className="bg-stone-900 text-white">
-      <div className="flex items-center">
-        <Link to="/" className="ml-8">
+    <nav className="bg-stone-900 text-white sans-serif flex justify-between w-full pr-16 pl-16  ">
+      <div className="flex items-center ">
+        <Link to="/">
           <img src={logo} alt="logo" style={{ width: "100px" }} />
         </Link>
         <div className="inline-flex w-30 h-14 mt-6">
           <Link
             to="/fandoms"
-            className="mr-4 ml-4 p-4 font-semibold hover:bg-white hover:text-stone-800"
+            className="mr-2 ml-6 p-4 font-semibold hover:bg-white hover:text-stone-800"
           >
             FANDOMS
           </Link>
@@ -32,30 +27,36 @@ function NavBar() {
           >
             CATEGORY
           </Link>
+          <div>
+            <ul>
+              <li></li>
+            </ul>
+          </div>
         </div>
-        <div className="flex-grow"></div> {/* Empty div for flexible space */}
-        <div className="flex items-center mr-8">
-          <input
-            type="search"
-            className={`text-stone-900 mr-16 rounded-full outline-0 transition-all duration-300 ${
-              isSearchFocused ? "w-64" : ""
-            }`}
-            placeholder={searchValue ? "" : "Search"}
-            onFocus={() => setIsSearchFocused(true)}
-            onBlur={() => setIsSearchFocused(false)}
-            onChange={handleInputChange}
-            style={{ paddingLeft: "1rem" }}
-          />
-          <Link to="/wishlist" className="mr-4">
-            <img src={heart} alt="heart" style={{ width: "45px" }} />
-          </Link>
-          <Link to="/login" className="mr-4">
-            <img src={user} alt="user" style={{ width: "40px" }} />
-          </Link>
-          <Link to="/cart" className="">
-            <img src={cart} alt="cart" style={{ width: "40px" }} />
-          </Link>
-        </div>
+      </div>
+      <div className="flex items-center gap-7">
+        <input
+          type="search"
+          className={`text-stone-900 rounded-full outline-0 transition-all duration-300   ${
+            isSearchFocused ? "w-64" : ""
+          }`}
+          placeholder={"SEARCH"}
+          onFocus={() => setIsSearchFocused(true)}
+          onBlur={() => setIsSearchFocused(false)}
+          style={{
+            paddingLeft: "1rem",
+            fontFamily: "sans-serif",
+          }}
+        />
+        <Link to="/wishlist" className="">
+          <img src={heart} alt="heart" style={{ width: "40px" }} />
+        </Link>
+        <Link to="/login" className="">
+          <img src={user} alt="user" style={{ width: "30px" }} />
+        </Link>
+        <Link to="/cart" className="">
+          <img src={cart} alt="cart" style={{ width: "40px" }} />
+        </Link>
       </div>
     </nav>
   );
