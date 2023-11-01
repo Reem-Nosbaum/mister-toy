11c0805b4f4bbc0a6a90cf86197b4d08f6e1d059
Reem-Nosbaum <reemnu@gmail.com>
2023-11-01 12:49:24 +0200

    Improve structure / format of the code. :art:

--- frontend-mister-toy/src/components/ImageSlider.jsx
+++ frontend-mister-toy/src/components/ImageSlider.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import rightArrow from "../assets/images/chevron-right.svg";
+import leftArrow from "../assets/images/chevron-left.svg";
 
 const ImageSlider = ({ slides }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -30,18 +32,18 @@ const ImageSlider = ({ slides }) => {
   }, [currentIndex]);
 
   return (
-    <div className="img div h-[490px] w-full relative">
+    <div className="img div h-[650px] w-full relative">
       <div
         className="absolute top-1/2 transform -translate-y-1/2 right-[32px] text-stone-50 z-10 cursor-pointer"
         onClick={nextSlide}
       >
-        ➡️
+        <img src={rightArrow} alt="right-arrow" className="w-10 h-10" />
       </div>
       <div
         className="absolute top-1/2 transform -translate-y-1/2 left-[32px] text-stone-50 z-10 cursor-pointer"
         onClick={prevSlide}
       >
-        ⬅️
+        <img src={leftArrow} alt="left-arrow" className="w-10 h-10 " />
       </div>
       <img
         className="w-full h-full"
--- frontend-mister-toy/src/pages/Home.jsx
+++ frontend-mister-toy/src/pages/Home.jsx
@@ -1,20 +1,26 @@
 import ImageSlider from "../components/ImageSlider";
-import image1 from "../assets/images/sliderImages/image-1.jpg";
-import image2 from "../assets/images/sliderImages/image-2.jpg";
-import image3 from "../assets/images/sliderImages/image-3.jpg";
-import image4 from "../assets/images/sliderImages/image-4.jpg";
+import image1 from "../assets/images/cover/Feature-FNAFHoliday-Background.jpg";
+import image2 from "../assets/images/cover/Feature-GiftGuide-Background.jpg";
+import image3 from "../assets/images/cover/Feature-OnePiece-Map-BG.jpg";
 
 function Home() {
   const slides = [
-    { img: image1, title: "beach" },
-    { img: image2, title: "city" },
-    { img: image3, title: "forest" },
-    { img: image4, title: "israel" },
+    { img: image1, title: "Holiday" },
+    { img: image2, title: "GiftGuide" },
+    { img: image3, title: "OnePiece" },
   ];
 
   return (
-    <div>
+    <div className="">
       <ImageSlider slides={slides} />
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-4xl font-bold z-10">
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt
+          nulla nisi veniam, vel qui quasi modi! Deleniti libero molestiae omnis
+          aspernatur, eaque possimus saepe nemo obcaecati, dolores, qui iure
+          maiores?
+        </div>
+      </div>
     </div>
   );
 }
