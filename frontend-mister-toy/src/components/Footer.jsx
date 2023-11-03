97264e5fc85a7e5f88df7796fea83dd8fb767750
Reem-Nosbaum <reemnu@gmail.com>
2023-11-03 12:13:18 +0200

    Improve structure / format of the code. :art:

--- frontend-mister-toy/src/components/Footer.jsx
+++ frontend-mister-toy/src/components/Footer.jsx
@@ -1,5 +1,13 @@
+import footer from "../assets/images/funko-rewardsbanner-herov9-exclusive-bg.png";
+
 function Footer() {
-  return <div className="text-3xl font-bold ">Footer</div>;
+  return (
+    <div className="text-white sans-serif">
+      <img src={footer} alt="logo" />
+      <div className="bg-stone-950 h-24">test</div>
+      <h1 className=" relative bottom-6">JOIN OUR FAN REWARDS PROGRAM</h1>
+    </div>
+  );
 }
 
 export default Footer;
