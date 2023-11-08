26b39d9f3a6a2835e893694527227a11ec73e23f
Reem-Nosbaum <reemnu@gmail.com>
2023-11-08 16:51:15 +0200

    Improve structure / format of the code. :art:

--- frontend-mister-toy/src/components/Footer.jsx
+++ frontend-mister-toy/src/components/Footer.jsx
@@ -1,19 +1,23 @@
+import { Link } from "react-router-dom";
 import footer from "../assets/images/funko-rewardsbanner-herov9-exclusive-bg.png";
+import instagram from "../assets/images/instagram-fill.svg";
 
 function Footer() {
   return (
-    <div>
-      <div className="text-white sans-serif absolute">
-        <img src={footer} alt="logo" />
-        <h1 className="relative bottom-56 text-3xl left-7">
-          JOIN OUR FAN REWARDS PROGRAM
-        </h1>
-        <h2 className="relative bottom-52 text-xl left-7">
-          Enroll to Get the Ultimate Fan Experience! Earn Points and Redeem for
-          <br /> Exclusive Rewards.
-        </h2>
+    <div className="text-white sans-serif absolute">
+      <img src={footer} alt="footer_img" />
+      <h1 className="absolute bottom-52 text-4xl mx-10">
+        JOIN OUR FAN REWARDS PROGRAM
+      </h1>
+      <h2 className="absolute bottom-36 mx-10 text-lg">
+        Enroll to Get the Ultimate Fan Experience! Earn Points and Redeem for
+        <br /> Exclusive Rewards.
+      </h2>
+      <div className="bg-stone-950 flex ">
+        <Link to="https://funko.com/">
+          <img className="w-10 h-10" src={instagram} alt="instagram" />
+        </Link>
       </div>
-      <div className="bg-stone-100 relative top-64 h-24"> instegram</div>
     </div>
   );
 }
