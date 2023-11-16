5f7b707291450d6a93cdf35916ead208c23926c5
Reem-Nosbaum <reemnu@gmail.com>
2023-11-16 13:53:59 +0200

    Improve structure / format of the code. :art:

--- frontend-mister-toy/src/pages/Fandoms.jsx
+++ frontend-mister-toy/src/pages/Fandoms.jsx
@@ -1,15 +1,49 @@
 import { useSelector } from "react-redux";
 import Card from "../components/Card";
+import { useState } from "react";
 
 function Fandoms() {
   const pops = useSelector((state) => state.pop.pops);
 
+  const [selectedCategory, setSelectedCategory] = useState("MARVEL");
+  const [selectedPrice, setSelectedPrice] = useState(12);
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const handlePriceChange = (event) => {
+    setSelectedPrice(Number(event.target.value));
+  };
+
+  const priceFilter = pops.filter((pop) => pop.price === selectedPrice);
+
+  console.log(priceFilter);
+
   return (
     <div className="">
-      Shop All
       <div className="bg-stone-100 p-10 flex flex-wrap ">
-        Fandoms
-        <Card pops={pops} startIndex={0} endIndex={20} />
+        <div className="p-3">
+          <select name="category" id="category" onChange={handleCategoryChange}>
+            <option value="MARVEL">MARVEL</option>
+            <option value="DRAGON BALL Z">DRAGON BALL Z</option>
+          </select>
+        </div>
+        <div className="p-3">
+          <select name="price" id="price" onChange={handlePriceChange}>
+            <option value={12}>12</option>
+            <option value={15}>15</option>
+          </select>
+        </div>
+
+        <Card
+          pops={pops.filter(
+            (pop) =>
+              pop.category === selectedCategory && pop.price === selectedPrice
+          )}
+          startIndex={0}
+          endIndex={20}
+        />
       </div>
     </div>
   );
