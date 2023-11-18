0fe7a1678dfd12f2e682661bcb9d96eaf7a00c0e
Reem-Nosbaum <reemnu@gmail.com>
2023-11-18 20:22:56 +0200

    Improve structure / format of the code. :art:

--- frontend-mister-toy/src/pages/Cart.jsx
+++ frontend-mister-toy/src/pages/Cart.jsx
@@ -1,4 +1,14 @@
 function Cart() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="text-4xl pb-10 pt-1">MY CART</div>
+      <div className="flex justify-between">
+        <h1 className=" text-sm font-serif flex items-start pl-2">ITEM</h1>
+        <h1 className=" text-sm font-serif">QTY</h1>
+        <h1 className=" text-sm font-serif right-96 relative">TOTAL</h1>
+      </div>
+      <div className="border-b border-stone-950 my-1"></div>
+    </div>
+  );
 }
 export default Cart;
