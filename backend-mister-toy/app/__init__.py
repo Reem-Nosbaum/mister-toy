81d1897397cdad3ef9b9444cf6bba46699e9d941
Reem-Nosbaum <reemnu@gmail.com>
2023-12-06 18:44:08 +0200

    Fix security or privacy issues. :lock:

--- backend-mister-toy/app/__init__.py
+++ backend-mister-toy/app/__init__.py
@@ -1,12 +1,16 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins=[
+    "http://localhost:5173",  
+    # "https://your-production-frontend.com",
+])
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# postgres://pop_storage_user:rw0xVfrFUS14kG2uGnAnC8rOpzuIbee4@dpg-clo4mvsjtl8s73ajra9g-a.oregon-postgres.render.com/pop_storage
 
 db = SQLAlchemy(app)
 
