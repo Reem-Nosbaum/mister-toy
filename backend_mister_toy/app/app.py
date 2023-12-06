a039dd91d29b63a0c271794d71ba92cab92ee27d
Reem-Nosbaum <reemnu@gmail.com>
2023-12-06 10:37:59 +0200

    Fix security or privacy issues. :lock:

--- backend_mister_toy/app/app.py
+++ backend_mister_toy/app/app.py
@@ -1,4 +1,38 @@
 
+# import os
+
+# from flask_cors import CORS
+# from routes import app, db
+
+# CORS(app, resources={
+#     r"/products/*": {"origins": "http://localhost:5173"},
+#     r"/slides/*": {"origins": "http://localhost:5173"}},
+#     allow_headers=["Content-Type", "Authorization"],
+#     supports_credentials=True,
+#     methods=["GET", "POST", "PUT", "DELETE"]
+# )
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")   
+
+
+# app.config['CORS_SUPPORTS_CREDENTIALS'] = True
+
+# db.init_app(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+
+
+# app.config['CORS_SUPPORTS_CREDENTIALS'] = True
+
+# db.init_app(app)
+
+# with app.app_context():
+#     db.create_all()
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+
 import os
 
 from flask_cors import CORS
@@ -7,8 +41,6 @@ from routes import app, db
 CORS(app, resources={
     r"/products/*": {"origins": "http://localhost:5173"},
     r"/slides/*": {"origins": "http://localhost:5173"}},
-    allow_headers=["Content-Type", "Authorization"],
-    supports_credentials=True,
     methods=["GET", "POST", "PUT", "DELETE"]
 )
 
@@ -18,15 +50,3 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['CORS_SUPPORTS_CREDENTIALS'] = True
 
 db.init_app(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
-
-
-app.config['CORS_SUPPORTS_CREDENTIALS'] = True
-
-db.init_app(app)
-
-with app.app_context():
-    db.create_all()
-
-if __name__ == '__main__':
-    app.run(debug=True)
