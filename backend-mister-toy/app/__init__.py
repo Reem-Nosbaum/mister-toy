2c155db1eaacea65f2993caf17dab85c9a574568
Reem-Nosbaum <reemnu@gmail.com>
2023-12-06 13:03:25 +0200

    Fix security or privacy issues. :lock:

--- backend-mister-toy/app/__init__.py
+++ backend-mister-toy/app/__init__.py
@@ -1,9 +1,12 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///products.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# postgres://pop_storage_user:rw0xVfrFUS14kG2uGnAnC8rOpzuIbee4@dpg-clo4mvsjtl8s73ajra9g-a.oregon-postgres.render.com/pop_storage
 
 db = SQLAlchemy(app)
 
