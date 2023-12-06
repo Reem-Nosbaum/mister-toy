7eb5edc47c16e3ff7c601351a1d35770f813e16f
Reem-Nosbaum <reemnu@gmail.com>
2023-12-06 18:48:12 +0200

    Fix security or privacy issues. :lock:

--- backend-mister-toy/app/__init__.py
+++ backend-mister-toy/app/__init__.py
@@ -4,10 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=[
-    "http://localhost:5173",  
-    # "https://your-production-frontend.com",
-])
+CORS(app, supports_credentials=True)
+
+# CORS(app, origins=[
+#     "http://localhost:5173",  
+#     # "https://your-production-frontend.com",
+# ])
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
